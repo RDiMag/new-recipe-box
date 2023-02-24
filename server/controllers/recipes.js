332f6a1d0676195194f435f9c4800a837c0a8c1a
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-02-23 20:06:43 -0500

    added comments on questions

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
 
+//Is it correct to do the API call within this controller file, and the render(recipes.ejs) part in the router file?
+
 module.exports = {
   getRecipes: ('/recipes/:query', async (req, res) => {
 
   try {
       const response = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${req.params.query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
       console.log(response.data.hits)
+      // Does this render call look okay?
       res.render('recipes', { recipe : response.data});
     
     
